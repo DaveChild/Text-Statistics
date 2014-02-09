9913e948e2ea728d9e1e772b03be0452f48aa827
jrfnl <github_nospam@adviesenzo.nl>
2014-02-09 01:06:07 +0100

    Use bc_math for floating point calculations for reliable results
    Ref: issue #14

--- TextStatistics.php
+++ TextStatistics.php
@@ -58,7 +58,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = round((206.835 - (1.015 * $this->average_words_per_sentence($strText)) - (84.6 * $this->average_syllables_per_word($strText))), 1);
+        $score = self::bc_calc(206.835, '-', self::bc_calc(self::bc_calc(1.015, '*', $this->average_words_per_sentence($strText)), '-', self::bc-calc(84.6, '*', $this->average_syllables_per_word($strText))), true, 1);
 
         return self::normalize_score($score, 0, 100);
     }
@@ -71,7 +71,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = round(((0.39 * $this->average_words_per_sentence($strText)) + (11.8 * $this->average_syllables_per_word($strText)) - 15.59), 1);
+        $score = self::bc_calc(self::bc_calc(0.39, '*', $this->average_words_per_sentence($strText)), '+', self::bc_calc(self::bc_calc(11.8, '*', $this->average_syllables_per_word($strText)), '-', 15.59), true, 1);
 
         return self::normalize_score($score, 0, 12);
     }
@@ -84,7 +84,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = round((($this->average_words_per_sentence($strText) + $this->percentage_words_with_three_syllables($strText, false)) * 0.4), 1);
+        $score = self::bc_calc($this->average_words_per_sentence($strText), '+', self::bc_calc($this->percentage_words_with_three_syllables($strText, false), '*', 0.4), true, 1);
 
         return self::normalize_score($score, 0, 19);
     }
@@ -97,7 +97,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = round(((5.89 * ($this->letter_count($strText) / $this->word_count($strText))) - (0.3 * ($this->sentence_count($strText) / $this->word_count($strText))) - 15.8 ), 1);
+        $score = self::bc_calc(self::bc_calc(5.89, '*', self::bc_calc($this->letter_count($strText), '/', $this->word_count($strText))), '-', self::bc_calc(self::bc_calc(0.3, '*', self::bc_calc($this->sentence_count($strText), '/', $this->word_count($strText))), '-', 15.8 ), true, 1);
 
         return self::normalize_score($score, 0, 12);
     }
@@ -110,7 +110,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = round(1.043 * sqrt(($this->words_with_three_syllables($strText) * (30 / $this->sentence_count($strText))) + 3.1291), 1);
+        $score = self::bc_calc(1.043, '*', sqrt(self::bc_calc(self::bc_calc($this->words_with_three_syllables($strText), '*', self::bc_calc(30, '/', $this->sentence_count($strText))), '+', 3.1291)), true, 1);
 
         return self::normalize_score($score, 0, 12);
     }
@@ -123,7 +123,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = round(((4.71 * ($this->letter_count($strText) / $this->word_count($strText))) + (0.5 * ($this->word_count($strText) / $this->sentence_count($strText))) - 21.43), 1);
+        $score = self::bc_calc(self::bc_calc(4.71, '*', self::bc_calc($this->letter_count($strText), '/', $this->word_count($strText))), '+', self::bc_calc(self::bc_calc(0.5, '*', self::bc_calc($this->word_count($strText), '/', $this->sentence_count($strText))), '-', 21.43), true, 1);
 
         return self::normalize_score($score, 0, 12);
     }
@@ -400,7 +400,7 @@ class TextStatistics
         $strText = $this->clean_text($strText);
         $intWordCount = $this->word_count($strText);
         $intLongWordCount = $this->words_with_three_syllables($strText, $blnCountProperNouns);
-        $intPercentage = (($intLongWordCount / $intWordCount) * 100);
+        $intPercentage = self::bc_calc(self::bc_calc($intLongWordCount, '/', $intWordCount), '*', 100);
 
         return ($intPercentage);
     }
@@ -513,7 +513,7 @@ class TextStatistics
     }
 
     /**
-     * Normalizes score accordin to min & max allowed. If score larger
+     * Normalizes score according to min & max allowed. If score larger
      * than max, max is returned. If score less than min, min is returned.
      * Thanks to github.com/lvil
      * @param   score        Initial score
@@ -523,11 +523,130 @@ class TextStatistics
     public static function normalize_score($score, $min, $max)
     {
         if ($score > $max) {
-                $score = $max;
+            $score = $max;
         } elseif ($score < $min) {
-                $score = $min;
+            $score = $min;
         }
 
         return $score;
     }
+    
+	/**
+	 * Do simple reliable floating point calculations without the risk of wrong results
+	 * @see http://floating-point-gui.de/
+	 * @see the big red warning on http://php.net/language.types.float.php
+	 *
+	 * @source https://gist.github.com/jrfnl/8449978
+	 *
+	 * In the rare case that the bcmath extension would not be loaded, it will return the normal calculation results
+	 *
+	 * @param	mixed	$number1	Scalar (string/int/float/bool)
+	 * @param	string	$action		Calculation action to execute. Valid input:
+	 *								'+' or 'add' or 'addition',
+	 *								'-' or 'sub' or 'subtract',
+	 *								'*' or 'mul' or 'multiply',
+	 *								'/' or 'div' or 'divide',
+	 *								'%' or 'mod' or 'modulus'
+	 *								'=' or 'comp' or 'compare'
+	 * @param	mixed	$number2    Scalar (string/int/float/bool)
+	 * @param	bool	$round		Whether or not to round the result. Defaults to false.
+	 *								Will be disregarded for a compare operation
+	 * @param	int		$decimals	Decimals for rounding operation. Defaults to 0.
+	 * @param	int		$precision	Calculation precision. Defaults to 10.
+	 * @return	mixed				Calculation result or false if either or the numbers isn't scalar or
+	 *								an invalid operation was passed
+	 *								- for compare the result will always be an integer
+	 *								- for all other operations, the result will either be an integer
+	 *								 (preferred) or a float
+	 */
+	public static function bc_calc( $number1, $action, $number2, $round = false, $decimals = 0, $precision = 10 )
+	{
+		if ( !is_scalar($number1) || !is_scalar($number2) ) {
+			return false;
+		}
+
+		$bc = extension_loaded( 'bcmath' );
+
+		if ($bc) {
+			$number1 = strval( $number1 );
+			$number2 = strval( $number2 );
+		}
+
+		$result  = null;
+		$compare = false;
+
+		switch ($action) {
+			case '+':
+			case 'add':
+			case 'addition':
+				$result = ($bc) ? bcadd( $number1, $number2, $precision ) /* string */ : ($number1 + $number2);
+				break;
+	
+			case '-':
+			case 'sub':
+			case 'subtract':
+				$result = ($bc) ? bcsub( $number1, $number2, $precision ) /* string */ : ($number1 - $number2);
+				break;
+	
+			case '*':
+			case 'mul':
+			case 'multiply':
+				$result = ($bc) ? bcmul( $number1, $number2, $precision ) /* string */ : ($number1 * $number2);
+				break;
+	
+			case '/':
+			case 'div':
+			case 'divide':
+				if ( $bc ) {
+					$result = bcdiv( $number1, $number2, $precision ); // string, or NULL if right_operand is 0
+				} else if ($number2 != 0) {
+					$result = $number1 / $number2;
+				}
+	
+				if ( !isset($result) ) {
+					$result = 0;
+				}
+				break;
+	
+			case '%':
+			case 'mod':
+			case 'modulus':
+				if ($bc) {
+					$result = bcmod( $number1, $number2, $precision ); // string, or NULL if modulus is 0.
+				} else if ($number2 != 0) {
+					$result = $number1 % $number2;
+				}
+	
+				if ( !isset($result) ) {
+					$result = 0;
+				}
+				break;
+	
+			case '=':
+			case 'comp':
+			case 'compare':
+				$compare = true;
+				if ($bc) {
+					$result = bccomp( $number1, $number2, $precision ); // returns int 0, 1 or -1
+				} else {
+					$result = ($number1 == $number2) ? 0 : ( ($number1 > $number2) ? 1 : -1 );
+				}
+				break;
+		}
+		
+		if ( isset($result) ) {
+			if ($compare === false) {
+				if ($round === true) {
+					$result = round( floatval($result), $decimals );
+					if ($decimals === 0) {
+						$result = (int) $result;
+					}
+				} else {
+					$result = ( intval($result) == $result ) ? intval($result) : floatval($result);
+				}
+			}
+			return $result;
+		}
+		return false;
+	}
 }
