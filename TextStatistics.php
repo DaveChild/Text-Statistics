6862cb95b4d92001d63feff31a5fd391afc1784b
DaveChild <dave@addedbytes.com>
2014-02-11 16:24:26 +0000

    Minor bug fixes and tidying.

--- TextStatistics.php
+++ TextStatistics.php
@@ -35,7 +35,7 @@
 class TextStatistics
 {
     protected $strEncoding = ''; // Used to hold character encoding to be used by object, if set
-    
+
     protected $blnMbstring = true; // Efficiency: Is the MB String extension loaded ?
 
     protected static $blnBcmath = true; // Efficiency: Is the BC Math extension loaded ?
@@ -52,7 +52,7 @@ class TextStatistics
             // Encoding is given. Use it!
             $this->strEncoding = $strEncoding;
         }
-        
+
         $this->blnMbstring = extension_loaded('mbstring');
         self::$blnBcmath = extension_loaded('bcmath');
     }
@@ -66,7 +66,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = self::bc_calc(206.835, '-', self::bc_calc(self::bc_calc(1.015, '*', $this->average_words_per_sentence($strText)), '-', self::bc-calc(84.6, '*', $this->average_syllables_per_word($strText))), true, 1);
+        $score = self::bc_calc(206.835, '-', self::bc_calc(self::bc_calc(1.015, '*', $this->average_words_per_sentence($strText)), '-', self::bc_calc(84.6, '*', $this->average_syllables_per_word($strText))), true, 1);
 
         return self::normalize_score($score, 0, 100);
     }
@@ -108,7 +108,7 @@ class TextStatistics
     {
         $strText = $this->clean_text($strText);
 
-        $score = self::bc_calc(self::bc_calc(5.89, '*', self::bc_calc($this->letter_count($strText), '/', $this->word_count($strText))), '-', self::bc_calc(self::bc_calc(0.3, '*', self::bc_calc($this->sentence_count($strText), '/', $this->word_count($strText))), '-', 15.8 ), true, 1);
+        $score = self::bc_calc(self::bc_calc(5.89, '*', self::bc_calc($this->letter_count($strText), '/', $this->word_count($strText))), '-', self::bc_calc(self::bc_calc(0.3, '*', self::bc_calc($this->sentence_count($strText), '/', $this->word_count($strText))), '-', 15.8), true, 1);
 
         return self::normalize_score($score, 0, 12);
     }
@@ -202,13 +202,13 @@ class TextStatistics
      */
     protected function clean_text($strText)
     {
-		static $clean = array();
-		
-		if (isset($clean[$strText])) {
-			return $clean[$strText];
-		}
+        static $clean = array();
 
-		$key = $strText;
+        if (isset($clean[$strText])) {
+            return $clean[$strText];
+        }
+
+        $key = $strText;
 
         // all these tags should be preceeded by a full stop.
         $fullStopTags = array('li', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'dd');
@@ -227,7 +227,7 @@ class TextStatistics
         $strText = preg_replace_callback('/\. [^ ]+/', create_function('$matches', 'return strtolower($matches[0]);'), $strText); // Lower case all words following terminators (for gunning fog score)
 
         $strText = trim($strText);
-        
+
         // Cache it and return
         $clean[$key] = $strText;
         return $strText;
@@ -360,7 +360,7 @@ class TextStatistics
         $intSentenceCount = $this->sentence_count($strText);
         $intWordCount = $this->word_count($strText);
 
-        return ( self::bc_calc($intWordCount, '/', $intSentenceCount) );
+        return (self::bc_calc($intWordCount, '/', $intSentenceCount));
     }
 
     /**
@@ -396,7 +396,7 @@ class TextStatistics
             $intSyllableCount += $this->syllable_count($arrWords[$i]);
         }
 
-        return ( self::bc_calc($intSyllableCount, '/', $intWordCount) );
+        return (self::bc_calc($intSyllableCount, '/', $intWordCount));
     }
 
     /**
@@ -571,121 +571,117 @@ class TextStatistics
 
         return $score;
     }
-    
-	/**
-	 * Do simple reliable floating point calculations without the risk of wrong results
-	 * @see http://floating-point-gui.de/
-	 * @see the big red warning on http://php.net/language.types.float.php
-	 *
-	 * @source https://gist.github.com/jrfnl/8449978
-	 *
-	 * In the rare case that the bcmath extension would not be loaded, it will return the normal calculation results
-	 *
-	 * @param   mixed   $number1	Scalar (string/int/float/bool)
-	 * @param   string  $action		Calculation action to execute. Valid input:
-	 *								'+' or 'add' or 'addition',
-	 *								'-' or 'sub' or 'subtract',
-	 *								'*' or 'mul' or 'multiply',
-	 *								'/' or 'div' or 'divide',
-	 *								'%' or 'mod' or 'modulus'
-	 *								'=' or 'comp' or 'compare'
-	 * @param   mixed   $number2    Scalar (string/int/float/bool)
-	 * @param   bool    $round		Whether or not to round the result. Defaults to false.
-	 *								Will be disregarded for a compare operation
-	 * @param   int     $decimals	Decimals for rounding operation. Defaults to 0.
-	 * @param   int     $precision	Calculation precision. Defaults to 10.
-	 * @return  mixed				Calculation result or false if either or the numbers isn't scalar or
-	 *								an invalid operation was passed
-	 *								- for compare the result will always be an integer
-	 *								- for all other operations, the result will either be an integer
-	 *								 (preferred) or a float
-	 */
-	public static function bc_calc( $number1, $action, $number2, $round = false, $decimals = 0, $precision = 10 )
-	{
-		if ( !is_scalar($number1) || !is_scalar($number2) ) {
-			return false;
-		}
 
-		if (self::blnBcmath) {
-			$number1 = strval( $number1 );
-			$number2 = strval( $number2 );
-		}
+    /**
+     * Do simple reliable floating point calculations without the risk of wrong results
+     * @see http://floating-point-gui.de/
+     * @see the big red warning on http://php.net/language.types.float.php
+     *
+     * @source https://gist.github.com/jrfnl/8449978
+     *
+     * In the rare case that the bcmath extension would not be loaded, it will return the normal calculation results
+     *
+     * @param   mixed   $number1    Scalar (string/int/float/bool)
+     * @param   string  $action        Calculation action to execute. Valid input:
+     *                                '+' or 'add' or 'addition',
+     *                                '-' or 'sub' or 'subtract',
+     *                                '*' or 'mul' or 'multiply',
+     *                                '/' or 'div' or 'divide',
+     *                                '%' or 'mod' or 'modulus'
+     *                                '=' or 'comp' or 'compare'
+     * @param   mixed   $number2    Scalar (string/int/float/bool)
+     * @param   bool    $round        Whether or not to round the result. Defaults to false.
+     *                                Will be disregarded for a compare operation
+     * @param   int     $decimals    Decimals for rounding operation. Defaults to 0.
+     * @param   int     $precision    Calculation precision. Defaults to 10.
+     * @return  mixed                Calculation result or false if either or the numbers isn't scalar or
+     *                                an invalid operation was passed
+     *                                - for compare the result will always be an integer
+     *                                - for all other operations, the result will either be an integer
+     *                                 (preferred) or a float
+     */
+    public static function bc_calc($number1, $action, $number2, $round = false, $decimals = 0, $precision = 10)
+    {
+        if (!is_scalar($number1) || !is_scalar($number2)) {
+            return false;
+        }
 
-		$result  = null;
-		$compare = false;
+        if (self::$blnBcmath) {
+            $number1 = strval($number1);
+            $number2 = strval($number2);
+        }
 
-		switch ($action) {
-			case '+':
-			case 'add':
-			case 'addition':
-				$result = (self::blnBcmath) ? bcadd( $number1, $number2, $precision ) /* string */ : ($number1 + $number2);
-				break;
-	
-			case '-':
-			case 'sub':
-			case 'subtract':
-				$result = (self::blnBcmath) ? bcsub( $number1, $number2, $precision ) /* string */ : ($number1 - $number2);
-				break;
-	
-			case '*':
-			case 'mul':
-			case 'multiply':
-				$result = (self::blnBcmath) ? bcmul( $number1, $number2, $precision ) /* string */ : ($number1 * $number2);
-				break;
-	
-			case '/':
-			case 'div':
-			case 'divide':
-				if (self::blnBcmath) {
-					$result = bcdiv( $number1, $number2, $precision ); // string, or NULL if right_operand is 0
-				} else if ($number2 != 0) {
-					$result = $number1 / $number2;
-				}
-	
-				if ( !isset($result) ) {
-					$result = 0;
-				}
-				break;
-	
-			case '%':
-			case 'mod':
-			case 'modulus':
-				if (self::blnBcmath) {
-					$result = bcmod( $number1, $number2, $precision ); // string, or NULL if modulus is 0.
-				} else if ($number2 != 0) {
-					$result = $number1 % $number2;
-				}
-	
-				if ( !isset($result) ) {
-					$result = 0;
-				}
-				break;
-	
-			case '=':
-			case 'comp':
-			case 'compare':
-				$compare = true;
-				if (self::blnBcmath) {
-					$result = bccomp( $number1, $number2, $precision ); // returns int 0, 1 or -1
-				} else {
-					$result = ($number1 == $number2) ? 0 : ( ($number1 > $number2) ? 1 : -1 );
-				}
-				break;
-		}
-		
-		if ( isset($result) ) {
-			if ($compare === false) {
-				if ($round === true) {
-					$result = round( floatval($result), $decimals );
-					if ($decimals === 0) {
-						$result = (int) $result;
-					}
-				} else {
-					$result = ( intval($result) == $result ) ? intval($result) : floatval($result);
-				}
-			}
-			return $result;
-		}
-		return false;
-	}
+        $result  = null;
+        $compare = false;
+
+        switch ($action) {
+            case '+':
+            case 'add':
+            case 'addition':
+                $result = (self::$blnBcmath) ? bcadd($number1, $number2, $precision) /* string */ : ($number1 + $number2);
+                break;
+            case '-':
+            case 'sub':
+            case 'subtract':
+                $result = (self::$blnBcmath) ? bcsub($number1, $number2, $precision) /* string */ : ($number1 - $number2);
+                break;
+            case '*':
+            case 'mul':
+            case 'multiply':
+                $result = (self::$blnBcmath) ? bcmul($number1, $number2, $precision) /* string */ : ($number1 * $number2);
+                break;
+            case '/':
+            case 'div':
+            case 'divide':
+                if (self::$blnBcmath) {
+                    $result = bcdiv($number1, $number2, $precision); // string, or NULL if right_operand is 0
+                } else if ($number2 != 0) {
+                    $result = $number1 / $number2;
+                }
+
+                if (!isset($result)) {
+                    $result = 0;
+                }
+                break;
+            case '%':
+            case 'mod':
+            case 'modulus':
+                if (self::$blnBcmath) {
+                    $result = bcmod($number1, $number2, $precision); // string, or NULL if modulus is 0.
+                } else if ($number2 != 0) {
+                    $result = $number1 % $number2;
+                }
+
+                if (!isset($result)) {
+                    $result = 0;
+                }
+                break;
+            case '=':
+            case 'comp':
+            case 'compare':
+                $compare = true;
+                if (self::$blnBcmath) {
+                    $result = bccomp($number1, $number2, $precision); // returns int 0, 1 or -1
+                } else {
+                    $result = ($number1 == $number2) ? 0 : (($number1 > $number2) ? 1 : -1);
+                }
+                break;
+        }
+
+        if (isset($result)) {
+            if ($compare === false) {
+                if ($round === true) {
+                    $result = round(floatval($result), $decimals);
+                    if ($decimals === 0) {
+                        $result = (int) $result;
+                    }
+                } else {
+                    $result = (intval($result) == $result) ? intval($result) : floatval($result);
+                }
+            }
+            return $result;
+        }
+
+        return false;
+    }
 }
